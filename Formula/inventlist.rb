3efff27637c53378fe989109d831b228a85e45e9
InventList Release Automation <releases@inventlist.com>
2026-08-13 15:20:20 +0000

    Update InventList CLI formula
    Release-Transaction: a4dedc83faaf8ea054794db2b0b129434eb1e0adb180a002cb02877cfbd20d41

--- Formula/inventlist.rb
+++ Formula/inventlist.rb
@@ -4,26 +4,26 @@
 class Inventlist < Formula
   desc "Command-line tools for publishing and operating projects"
   homepage "https://inventlist.com"
-  version "0.59.1"
+  version "0.59.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/inventlist/cli-releases/releases/download/v0.59.1/inventlist_Darwin_arm64.tar.gz"
-      sha256 "51c0136019140cc9d766b48758cb72dcc097bbcf60a18cca5d08268b85405ac8"
+      url "https://github.com/inventlist/cli-releases/releases/download/v0.59.2/inventlist_Darwin_arm64.tar.gz"
+      sha256 "bd247a4aa2ffd009acbac9221809568bb3239bd00cc0b180998fded9fbef6545"
     end
     on_intel do
-      url "https://github.com/inventlist/cli-releases/releases/download/v0.59.1/inventlist_Darwin_x86_64.tar.gz"
-      sha256 "3a2a2d3448474d31c0180bd3d9630abaa39530e6e061fa8b98e6c82482475efa"
+      url "https://github.com/inventlist/cli-releases/releases/download/v0.59.2/inventlist_Darwin_x86_64.tar.gz"
+      sha256 "cb6800eabd4fde041a6bbbb8d100591599ec9002e82ab0935e1a5904ac8cfecd"
     end
   end
   on_linux do
     on_arm do
-      url "https://github.com/inventlist/cli-releases/releases/download/v0.59.1/inventlist_Linux_arm64.tar.gz"
-      sha256 "347dea3d2d067f07b7fc17f811106d633704914b6e855b50953e7a678fd87519"
+      url "https://github.com/inventlist/cli-releases/releases/download/v0.59.2/inventlist_Linux_arm64.tar.gz"
+      sha256 "cf9209cb929b8d3c74eff270138e866c994062d73b357b85205bc269a559af23"
     end
     on_intel do
-      url "https://github.com/inventlist/cli-releases/releases/download/v0.59.1/inventlist_Linux_x86_64.tar.gz"
-      sha256 "dab12d7dc52dbe2ac0fc3076ed038bd933954cdc52f082ee184a0028da10ed95"
+      url "https://github.com/inventlist/cli-releases/releases/download/v0.59.2/inventlist_Linux_x86_64.tar.gz"
+      sha256 "f3379711b03d1f181d4188d5bd11509620b793ba7baac03c1d1ed5365c7aaf11"
     end
   end
 
@@ -33,7 +33,7 @@ class Inventlist < Formula
   end
 
   test do
-    assert_equal "inventlist v0.59.1", shell_output("#{bin}/inventlist version").strip
-    assert_equal "inventlist v0.59.1", shell_output("#{bin}/i version").strip
+    assert_equal "inventlist v0.59.2", shell_output("#{bin}/inventlist version").strip
+    assert_equal "inventlist v0.59.2", shell_output("#{bin}/i version").strip
   end
 end
